b881ddf89160cce1300162926de563f4032718f8
John McIver <john.mciver.iii@gmail.com>
2020-08-05 23:28:10 -0600

    Update acceptance tests to use supported version of Developer Toolset

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -16,7 +16,7 @@ describe 'scldevtoolset class' do
       it { is_expected.to be_installed }
     end
 
-    Array(3..7).each do |version|
+    [7, 9].each do |version|
       describe package("devtoolset-#{version}") do
         it { is_expected.not_to be_installed }
       end
@@ -34,44 +34,37 @@ describe 'scldevtoolset class' do
       apply_manifest(pp, catch_failures: true)
     end
 
-    Array(7..8).each do |version|
+    [7, 8].each do |version|
       describe package("devtoolset-#{version}") do
         it { is_expected.to be_installed }
       end
     end
 
-    Array(3..6).each do |version|
+    [9].each do |version|
       describe package("devtoolset-#{version}") do
         it { is_expected.not_to be_installed }
       end
     end
   end
 
-  context 'versions = [6, 8]' do
-    it 'verify version install and removal' do
-      pp1 = <<-EOS
+  context 'versions = [9]' do
+    it 'verify single version install' do
+      pp = <<-EOS
         class { 'scldevtoolset':
-          versions => [7, 8]
+          versions => [9]
         }
       EOS
 
-      pp2 = <<-EOS
-        class { 'scldevtoolset':
-          versions => [6, 8]
-        }
-      EOS
-
-      apply_manifest(pp1, catch_failures: true)
-      apply_manifest(pp2, catch_failures: true)
+      apply_manifest(pp, catch_failures: true)
     end
 
-    [6, 8].each do |version|
+    [9].each do |version|
       describe package("devtoolset-#{version}") do
         it { is_expected.to be_installed }
       end
     end
 
-    [3, 4, 5, 7].each do |version|
+    [7, 8].each do |version|
       describe package("devtoolset-#{version}") do
         it { is_expected.not_to be_installed }
       end
