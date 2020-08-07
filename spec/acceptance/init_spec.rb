aa77bff9ee8e4c689eefdadd41a8cdeadbc8b99f
John McIver <john.mciver.iii@gmail.com>
2020-08-07 17:07:04 -0600

    Adding acceptance tests for environment module feature

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'scldevtoolset class' do
-  context 'versions = default' do
+  context 'versions argument is not specified' do
     it 'verify idempotency' do
       pp = <<-EOS
         include scldevtoolset
@@ -50,6 +50,71 @@ describe 'scldevtoolset class' do
       describe package("devtoolset-#{version}") do
         it { is_expected.to be_installed }
       end
+    end
+  end
+
+  context 'versions = [7, 8, 9] with environment modules' do
+    let(:module_path) { '/etc/modulefiles/scl-devtools' }
+
+    it 'verify multiple version install' do
+      pp = <<-EOS
+        class { 'scldevtoolset':
+          versions => [7, 8, 9],
+          use_modules => true,
+          install_environment_modules => true,
+        }
+      EOS
+
+      apply_manifest(pp, catch_failures: true)
+    end
+
+    it 'install directory /etc/modulefiles/scl-devtools' do
+      expect(shell('[ -d /etc/modulefiles/scl-devtools ];').exit_code).to eq 0
+    end
+
+    it 'install file /etc/modulefiles/scl-devtools/.base' do
+      expect(shell('[ -f /etc/modulefiles/scl-devtools/.base ];').exit_code).to eq 0
+    end
+
+    [7, 8, 9].each do |version|
+      it "install symlink /etc/modulefiles/scl-devtools/#{version} to /etc/modulefiles/scl-devtools/.base" do
+        expect(
+          shell(
+            "[ -h /etc/modulefiles/scl-devtools/#{version} ] && [ $(readlink /etc/modulefiles/scl-devtools/#{version}) = '/etc/modulefiles/scl-devtools/.base' ];",
+          ).exit_code,
+        ).to eq 0
+      end
+
+      it "module load devtools-#{version}" do
+        expect(
+          shell(
+            "module load scl-devtools/#{version}; gcc --version | head -n 1 |  cut -d \" \" -f3 | cut -d \".\" -f1;",
+          ).stdout.strip,
+        ).to eq version.to_s
+      end
+
+      describe package("devtoolset-#{version}") do
+        it { is_expected.to be_installed }
+      end
+    end
+  end
+
+  context 'versions = [9] with environment modules' do
+    it 'verify multiple version install' do
+      pp = <<-EOS
+        package { 'environment-modules':
+          ensure => present,
+        }
+
+        class { 'scldevtoolset':
+          versions => [7, 8, 9],
+          use_modules => true,
+          install_environment_modules => false,
+          require => Package['environment-modules'],
+        }
+      EOS
+
+      apply_manifest(pp, catch_failures: true)
     end
   end
 end
